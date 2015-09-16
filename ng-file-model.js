c108d42d1299cd846e065ed24011e062c9b1b286
TheoSqua <theosqua@gmail.com>
2015-09-16 14:44:04 -0500

    Update ng-file-model.js
    Modified directive to work with multiple file attachments.

--- ng-file-model.js
+++ ng-file-model.js
@@ -1,29 +1,31 @@
 (function () {
-    'use strict';
-    angular.module('ng-file-model', [])
-    .directive("ngFileModel", [function () {
-        return {
-            scope: {
-                ngFileModel: "="
-            },
-            link: function (scope, element, attributes) {
-                element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
-                        scope.$apply(function () {
-                            scope.ngFileModel = {
-                                lastModified: changeEvent.target.files[0].lastModified,
-                                lastModifiedDate: changeEvent.target.files[0].lastModifiedDate,
-                                name: changeEvent.target.files[0].name,
-                                size: changeEvent.target.files[0].size,
-                                type: changeEvent.target.files[0].type,
-                                data: loadEvent.target.result
-                            };
-                        });
-                    }
-                    reader.readAsDataURL(changeEvent.target.files[0]);
-                });
-            }
-        }
-    }]);
-})();
+	angular.module('ng-file-model', [])
+	.directive("ngFileModel", [function () {
+		return {
+			scope: {
+				ngFileModel: "="
+			},
+			link: function (scope, element, attributes) {
+				element.bind("change", function (changeEvent) {
+					var reader = new FileReader();
+					reader.onload = function (loadEvent) {
+						scope.$apply(function () {
+							for (var i=0; i< changeEvent.target.files.length; i++) {
+								var eventSplit = loadEvent.target.result.split(',');
+								var ngFileModel = {
+									lastModified: changeEvent.target.files[i].lastModified,
+									lastModifiedDate: changeEvent.target.files[i].lastModifiedDate,
+									name: changeEvent.target.files[i].name,
+									size: changeEvent.target.files[i].size,
+									type: changeEvent.target.files[i].type,
+									data: eventSplit[1]
+								};
+								scope.ngFileModel.push(ngFileModel);
+							}
+							element.value = '';
+						});
+					};
+					reader.readAsDataURL(changeEvent.target.files[0]);
+				});
+			}
+		};
