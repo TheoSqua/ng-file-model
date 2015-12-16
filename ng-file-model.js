70b67d94a7420422417333b6e918ce07eb17efb9
TheoSqua <theosqua@gmail.com>
2015-12-16 13:20:38 -0600

    Update ng-file-model.js
    Directive works better with multiple files.

--- ng-file-model.js
+++ ng-file-model.js
@@ -6,26 +6,35 @@
 				ngFileModel: "="
 			},
 			link: function (scope, element, attributes) {
-				element.bind("change", function (changeEvent) {
-					var reader = new FileReader();
-					reader.onload = function (loadEvent) {
-						scope.$apply(function () {
-							for (var i=0; i< changeEvent.target.files.length; i++) {
-								var eventSplit = loadEvent.target.result.split(',');
-								var ngFileModel = {
-									lastModified: changeEvent.target.files[i].lastModified,
-									lastModifiedDate: changeEvent.target.files[i].lastModifiedDate,
-									name: changeEvent.target.files[i].name,
-									size: changeEvent.target.files[i].size,
-									type: changeEvent.target.files[i].type,
-									data: eventSplit[1]
+				element.bind("change", function (changeEvent) {					
+					for (var i=0; i< changeEvent.target.files.length; i++) {
+						setupReader(changeEvent.target.files[i]);
+					}
+
+					function setupReader(file) {
+						var fileObj = {};
+						var reader = new FileReader();
+						var target = '';
+						reader.onload = function(e) {
+							scope.$apply(function () {
+								var eSplit = e.target.result.split(',');
+								fileObj = {
+									lastModified: file.lastModified,
+									lastModifiedDate: file.lastModifiedDate,
+									name: file.name,
+									size: file.size,
+									type: file.type,
+									data: eSplit[1]
 								};
-								scope.ngFileModel.push(ngFileModel);
-							}
-							element.value = '';
-						});
-					};
-					reader.readAsDataURL(changeEvent.target.files[0]);
+								scope.ngFileModel.push(fileObj);
+							});
+							element[0].value = '';
+						};
+						//this sets the value of e.target.results.
+						reader.readAsDataURL(file);
+					}
 				});
 			}
 		};
+	}]);
+})();
